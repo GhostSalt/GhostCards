6d2986a3e2c10fb56133362d25902e0315ba40ec
GhostSalt <mhanna756@c2ken.net>
2025-10-07 15:48:33 +0100

    -Ghost texture pack stuff lalala

--- GhostCards.lua
+++ GhostCards.lua
@@ -8,50 +8,73 @@
 --- VERSION: 1.0.0
 --- BADGE_COLOR: 888888
 
-local atlas_key = 'GS_GhostCards' -- Format: PREFIX_KEY
+local atlas_key = 'GS_GhostCards'                                                               -- Format: PREFIX_KEY
 -- See end of file for notes
-local atlas_path = 'GhostCards.png' -- Filename for the image in the asset folder
-local atlas_path_hc = 'GhostCardsHC.png' -- Filename for the high-contrast version of the texture, if existing
+local atlas_path =
+'GhostCards.png'                                                                                -- Filename for the image in the asset folder
+local atlas_path_hc =
+'GhostCardsHC.png'                                                                              -- Filename for the high-contrast version of the texture, if existing
 
-local suits = {'hearts', 'clubs', 'diamonds', 'spades'} -- Which suits to replace
-local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",} -- Which ranks to replace
+local suits = { 'hearts', 'clubs', 'diamonds', 'spades' }                                       -- Which suits to replace
+local ranks = { '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace", } -- Which ranks to replace
 
-local description = 'Ghost' -- English-language description, also used as default
+local description =
+'Ghost'                                                                                         -- English-language description, also used as default
 
 -----------------------------------------------------------
 -- You should only need to change things above this line --
 -----------------------------------------------------------
 
-SMODS.Atlas{  
-    key = atlas_key..'_lc',
+SMODS.Atlas {
+    key = atlas_key .. '_lc',
     px = 71,
     py = 95,
     path = atlas_path,
-    prefix_config = {key = false}, -- See end of file for notes
+    prefix_config = { key = false }, -- See end of file for notes
 }
 
 if atlas_path_hc then
-    SMODS.Atlas{  
-        key = atlas_key..'_hc',
+    SMODS.Atlas {
+        key = atlas_key .. '_hc',
         px = 71,
         py = 95,
         path = atlas_path_hc,
-        prefix_config = {key = false}, -- See end of file for notes
+        prefix_config = { key = false }, -- See end of file for notes
     }
 end
 
 for _, suit in ipairs(suits) do
-    SMODS.DeckSkin{
-        key = suit.."_skin",
+    SMODS.DeckSkin {
+        key = suit .. "_skin",
         suit = suit:gsub("^%l", string.upper),
         ranks = ranks,
-        lc_atlas = atlas_key..'_lc',
-        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
+        lc_atlas = atlas_key .. '_lc',
+        hc_atlas = (atlas_path_hc and atlas_key .. '_hc') or atlas_key .. '_lc',
         loc_txt = {
             ['en-us'] = description
         },
         posStyle = 'deck'
     }
+end
+
+if next(SMODS.find_mod('malverk')) then
+    AltTexture({
+        key = 'ghost_jokers',
+        set = 'Joker',
+        path = 'GhostJokers.png',
+        loc_txt = { name = 'Ghost Jokers' },
+        display_pos = 'j_banner',
+        original_sheet = true
+    })
+
+    TexturePack({
+        key = 'ghost_jokers_main',
+        textures = { 'GS_ghost_jokers' },
+        loc_txt = {
+            name = 'Ghost Jokers',
+            text = { 'A set of Jokers', 'featuring {C:dark_edition}Ghost{},', 'instead of {C:red}Jimbo{}!' }
+        }
+    })
 end
 
 -- Notes:
